fe0d2517927785a70a920abf4e7414fa099dd26f
vsuortiz-cmu-S25 <vsuortiz1@gmail.com>
2025-05-20 15:25:47 -0700

    description for teh task schema

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
+
 class TaskBase(BaseModel):
+    """
+    Schema for the task
+    """
+
     title: str
     description: Optional[str] = None
-
-
