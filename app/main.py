473542b7b69d9d7a9e0db9249472318b0f42f8d9
vsuortiz-cmu-S25 <vas64@cornell.edu>
2025-05-20 00:31:08 -0700

    add basic functionality

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI
 
 app = FastAPI(title="Task Management API")
+
+
+@app.get("/")
+async def root():
+    """
+    returns the main message
+    """
+    return {"Message", "Welcome to the task management app"}
+
+
+@app.get("/hello/{name}")
+async def hello(name: str):
+    """
+    test for api params
+    """
+    return {"Message", f"Hello {name}"}
