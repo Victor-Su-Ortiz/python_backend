6f5fc94fe0ccb4f069f3364ea3d4cdfd67acc09a
vsuortiz-cmu-S25 <vsuortiz1@gmail.com>
2025-05-20 15:48:53 -0700

    finish the schemas

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -9,3 +10,22 @@ class TaskBase(BaseModel):
 
     title: str
     description: Optional[str] = None
+    due_date: Optional[str] = None
+    priority: int = Field(gt=0, lt=6, description=("Priority between 1-5"))
+
+
+class TaskCreate(TaskBase):
+    """
+    class for creating a new task
+    """
+
+
+class TaskResponse(TaskBase):
+    """
+    for the response
+    """
+
+    id: int
+    created_at: datetime
+    owner_id: int
+    completed: bool = False
